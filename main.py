96505d54736f7b0bf2424486c5f43958e783e009
tezzytezzy <tezzy@hotmail.com>
2019-09-30 06:14:28 -0700

    cont.

--- main.py
+++ main.py
@@ -1,23 +1,62 @@
 from abc import ABCMeta, abstractmethod
 
-class Financial_Institution(metaclass=ABCMeta):
-    def __init__(self, name, address):
-        pass
 
-class Bank(Financial_Institution):
-    def __init__(self):
-        super().__init__()
-        self._is_online_only = False
+class FinancialInstitution(metaclass=ABCMeta):
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self._is_for_profit = True
+        self._balance = 0
+        self._is_atm_service = False
+        self._is_retail_banking = True
 
     def deposit_money(self, amount):
-        pass
+        self._balance += amount
 
     def withdraw_money(self, amount):
-        pass
+        if self._balance - amount < 0:
+            print("Your balance would go below zero (0) with a withdraw of ${0}!".format(amount))
+            print("Current Balance: ${0}".format(self._balance))
+        else:
+            self._balance -= amount
 
-    def transfer_to_another_fi(self, amount):
-        pass
+    def transfer_to_another_fi(self, fi, amount):
+        self.withdraw_money(amount)
+        self.show_balance
+        # How to prevent overwithdrawing???
+        fi.deposit_money(amount)
+
+    @property # getter, not a method
+    def show_balance(self):
+        return self._balance
 
 
+class Bank(FinancialInstitution):
+    def __init__(self, name, address):
+        super(Bank, self).__init__(name, address)
+        self._is_atm_service = True
 
 
+class CreditUnion(FinancialInstitution):
+    def __init__(self, name, address):
+        super(CreditUnion, self).__init__(name, address)
+        self._is_for_profit = False
+
+
+class Insurance(FinancialInstitution):
+    def __init__(self):
+        super(Insurance, self).__init__()
+        self._is_retail_banking = False
+
+
+bank_a = Bank("Bank A", "129 West 81st Street, Apartment 5A")
+print("Name: " + bank_a.name + " " + "Address: " + bank_a.address)
+
+bank_a.deposit_money(200)
+print(bank_a.show_balance)
+bank_a.withdraw_money(100)
+bank_a.withdraw_money(200)
+
+credit_a = CreditUnion("Credit Union A", "2707 Martin Luther King Jr Ave SE")
+bank_a.transfer_to_another_fi(credit_a, 50)
+print(credit_a.show_balance)
